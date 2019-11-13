64d2018edac1ffa50a14f1cc2397458953d6c3b6
Porco <zhangporco@gmail.com>
2019-11-13 20:16:47 +0800

    范型

--- types/lemon.d.ts
+++ types/lemon.d.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Porco
  * @Date: 2019-11-11 14:23:12
- * @LastEditTime: 2019-11-13 16:36:02
+ * @LastEditTime: 2019-11-13 20:15:29
  * @Description: 请填写文件注释
  */
 
@@ -12,13 +12,13 @@ export declare class LemonTree {
 
   hasNodeId(tree: object, nodeId: string): boolean;
 
-  getNodeById(tree: object, nodeId: string): object;
+  getNodeById<T>(tree: T, nodeId: string): T;
 
   findParentIds(tree: object, nodeId: string): Array<any>;
   
   findParents(tree: object, nodeId: string): Array<any>;
 
-  findTree(tree: object, nodeId: string): object;
+  findTree<T>(tree: T, nodeId: string): T;
 
   makeTree(array: Array<any>): object;
 
