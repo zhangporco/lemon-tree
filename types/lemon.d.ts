d06df72a3bab6e3c9a030c594ba2239036bcafdf
Porco <zhangporco@gmail.com>
2019-11-13 16:58:22 +0800

    ts 支持

--- types/lemon.d.ts
+++ types/lemon.d.ts
@@ -1,11 +1,13 @@
 /*
  * @Author: Porco
  * @Date: 2019-11-11 14:23:12
- * @LastEditTime: 2019-11-11 14:58:14
+ * @LastEditTime: 2019-11-13 16:36:02
  * @Description: 请填写文件注释
  */
 
 export declare class LemonTree {
+  constructor(option: object);
+
   findPidsByTrees(tree: object, nodeId: string): Array<any>;
 
   hasNodeId(tree: object, nodeId: string): boolean;
